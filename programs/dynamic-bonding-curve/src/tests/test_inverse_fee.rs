b385051c2e04d4781212b8c7cb517118e4b70056
codewithgun <codewithgun@gmail.com>
2025-07-15 11:32:45 +0800

    fix: prop-test

--- programs/dynamic-bonding-curve/src/tests/test_inverse_fee.rs
+++ programs/dynamic-bonding-curve/src/tests/test_inverse_fee.rs
@@ -1,13 +1,15 @@
 use crate::constants::fee::{MAX_FEE_NUMERATOR, MIN_FEE_NUMERATOR};
 use crate::state::PoolFeesConfig;
 use proptest::prelude::*;
+
 proptest! {
     #![proptest_config(ProptestConfig {
         cases: 10000, .. ProptestConfig::default()
     })]
     #[test]
     fn test_inverse_fee(
-        excluded_fee_amount in 0..=u64::MAX/2,
+        // u64::MAX * (FEE_DENOMINATOR - trade_fee_numerator) / FEE_DENOMINATOR - 1
+        excluded_fee_amount in 0..=184467440737095515u64,
         trade_fee_numerator in MIN_FEE_NUMERATOR..=MAX_FEE_NUMERATOR
     ){
         let included_fee_amount = PoolFeesConfig::get_included_fee_amount(trade_fee_numerator, excluded_fee_amount).unwrap();
